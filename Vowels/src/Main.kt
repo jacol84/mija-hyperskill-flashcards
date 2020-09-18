93844089c13514d0ffd832ef407137579790c10c
jac <devjac@ogarnij.se>
2020-09-18 18:12:16 +0200

    - Vowels

--- Vowels/src/Main.kt
+++ Vowels/src/Main.kt
@@ -1,8 +1,6 @@
 private val allChar = ('a'..'z').map { it }
 private fun Char.toPair(): Pair<Char, Int> = this to allChar.indexOf(this) + 1
-private val map: Map<Char, Int> = mapOf(
-        *arrayOf('a', 'e', 'u', 'i', 'o').map { it.toPair() }.toTypedArray()
-)
+private val map = arrayOf('a', 'e', 'u', 'i', 'o').map { it.toPair() }.toMap()
 
 fun main() {
     val letter = readLine()!!.toCharArray()[0].toLowerCase()
