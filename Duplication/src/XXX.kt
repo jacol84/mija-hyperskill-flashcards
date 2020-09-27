2bc909bc8f820a135b3b1723a248564f13ebb775
jac <devjac@ogarnij.se>
2020-09-27 11:28:51 +0200

    - array fix-size Accessing elements

--- Duplication/src/XXX.kt
+++ Duplication/src/XXX.kt
@@ -1,4 +1,4 @@
-fun main() {
+fun xxx() {
     val a = arrayOf(9, 8, 3, 1, 5, 4)
 
     for (i in a.indices) {
@@ -8,6 +8,26 @@ fun main() {
             a[i] -= 1
         }
     }
-    println(a.joinToString { ", " })
+    println(a.joinToString(separator = " "))
 
 }
+
+fun xxx2() {
+    val a = arrayOf(3, 5, 2, 1)
+    var x = 10
+    a[0] -= x
+    a[1] += x
+    x -= a[2]
+    a[2] = x
+    a[3] -= x
+
+    println(a.joinToString(separator = " "))
+
+}
+
+
+fun main() {
+    xxx()
+    xxx2()
+}
+
