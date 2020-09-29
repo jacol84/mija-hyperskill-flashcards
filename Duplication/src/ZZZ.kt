1d0556de69670b545fec3f2c444b147166e86be7
jac <devjac@ogarnij.se>
2020-09-29 10:39:50 +0200

    - Fixed-size array  Two arrays

--- Duplication/src/ZZZ.kt
+++ Duplication/src/ZZZ.kt
@@ -1,3 +1,13 @@
+val a = arrayOf(4, 0, 9, 2, 1)
+val b = arrayOf(6, 3, 2, 9, 0)
 
-class ZZZ {
+fun main() {
+    for (i in a.indices) {
+        if (i % 2 == 0)
+            a[i] -= b[i]
+        else
+            b[i] -= a[i]
+    }
+    println(a.joinToString(separator = " "))
+    println(b.joinToString(separator = " "))
 }
