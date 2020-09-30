c28e101fbf22b28ae818ebef6a8218e8d2c5fae1
jac <devjac@ogarnij.se>
2020-09-30 10:31:39 +0200

    - Handling strings

--- Converter to Double/src/Main.kt
+++ Converter to Double/src/Main.kt
@@ -1,6 +1,10 @@
 fun convertStringToDouble(input: String): Double {
     /**
-    * It returns a double value or 0 if an exception occurred
-    */
-    return input.toDouble()
+     * It returns a double value or 0 if an exception occurred
+     */
+    return try {
+        input.toDouble()
+    } catch (e: Exception) {
+        0.0
+    }
 }
