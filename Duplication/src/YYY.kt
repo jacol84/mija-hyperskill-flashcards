48377eb3cd1cd1246d6ddf2b82db6d6204ed42c6
jac <devjac@ogarnij.se>
2020-09-30 10:25:48 +0200

    - Exception message

--- Duplication/src/YYY.kt
+++ Duplication/src/YYY.kt
@@ -8,47 +8,4 @@ fun main() {
 
 fun main(args: Int) {
     print(args)
-
-    try {
-        // code that may throw an exception
-    }
-    catch (e: RuntimeException) {
-        // ...
-    }
-    catch (e: Exception) {
-        // ...
-    }
-
-    try {
-        // code that may throw an exception
-    }
-    catch (e: Exception) {
-        // ...
-    }
-    catch (e: RuntimeException) {
-        // ...
-    }
-
-    try {
-        // code that may throw an exception
-    }
-    catch (Exception) {
-        // ...
-    }
-    catch (RuntimeException) {
-        // ...
-    }
-
-    try {
-        // code that may throw an exception
-    }
-    catch 1: (e: ArithmeticException) {
-        // ...
-    }
-    catch 2: (e: RuntimeException) {
-        // ...
-    }
-    catch 3: (e: Exception) {
-        // ...
-    }
 }
